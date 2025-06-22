f3146b294fa013fa5a586e192be6af37be36b86f
Yereong <lily030624@gmail.com>
2025-06-23 04:50:48 +0900

    feat: 레시피 더보기 api 추가

---
+++ src/api/recipe/getRecipelist.tsx
@@ -0,0 +1,35 @@
+import axios, { AxiosResponse } from "axios";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export const GetRecipeList = async (
+  page:number
+): Promise<any> => {
+  axios.defaults.withCredentials = true;
+
+  const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
+
+  try {
+    const response: AxiosResponse<any> = await axios.get(
+      `${apiUrl}/api/recipes/list?page=${page}`,{
+      headers: accessToken
+        ? { Authorization: `Bearer ${accessToken}` }
+        : {},
+    }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      const { status, data } = error.response;
+      console.error("Error response:", status, data);
+      
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error setting up request:", error.message);
+    }
+
+    throw error;
+  }
+};
